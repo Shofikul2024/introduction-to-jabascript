da15fbe86b8ca5dbe9365fdd88b5e7195c2d0fd5
Shofikul2024 <islamshofi942@gmail.com>
2024-11-10 23:46:46 +0600

    first commit

---
+++ number.js
@@ -0,0 +1,13 @@
+var orangePrice = 20;
+var chocolatePrice  = 30;
+var applePrice = "26";
+
+console.log( chocolatePrice);
+ 
+
+
+ console.log( orangePrice);
+ console.log( orangePrice +  applePrice);
+ console.log(typeof  applePrice);
+
+
